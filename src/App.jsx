ad4d71bf135c36f2079b6e98c024336a6ace7a77
Derek Paul <dereksamuelgr@gmail.com>
2021-07-10 13:41:59 -0500

    add simple iteration

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import ClassPets from "./components/ItemClassPets/index.jsx";
+import ClassPets from "./components/ClassPets/index.jsx";
 
 const App = () => (
   <>
-    <ClassPets cover="https://news.cgtn.com/news/77416a4e3145544d326b544d354d444d3355444f31457a6333566d54/img/37d598e5a04344da81c76621ba273915/37d598e5a04344da81c76621ba273915.jpg"></ClassPets>
+    <ClassPets></ClassPets>
   </>
 );
 
--- src/components/ClassPets/index.jsx
+++ src/components/ClassPets/index.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import ItemClassPets from "../ItemClassPets/index.jsx";
+
+const listOfClasses = [1, 2, 3, 4, 5];
 
 export default function ClassPets() {
   return (
     <div>
-      
+      {
+        listOfClasses.map((classPet, index) => (
+          <ItemClassPets key={index} path={classPet} />
+        ))
+      }
     </div>
   );
 }
