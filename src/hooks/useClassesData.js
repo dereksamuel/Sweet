961e807cc9ad7736062777d9d5e24218fc9172eb
Derek Paul <dereksamuelgr@gmail.com>
2021-07-16 10:35:47 -0500

    changes to url

--- src/hooks/useClassesData.js
+++ src/hooks/useClassesData.js
@@ -4,7 +4,7 @@ function useClassesData() {
   const [classesList, setClassesList] = useState([]);
   const [loading, setLoading] = useState(true);
   useEffect(async () => {
-    const response = await (await fetch("https://sweet-server-vercel.vercel.app/categories")).json();
+    const response = await (await fetch("https://sweet-server-vercel-r6i1a35k4-dereksamuel.vercel.app/categories")).json();
     setClassesList(response);
     setLoading(false);
   }, []);
